98dbbc1c0eb389c97e7073aabf21d222faf1bf4a
Porter <slugsaver@gmail.com>
2018-07-02 09:05:19 -0700

    working on database endpoints

--- dnd.js
+++ dnd.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render(pages/login));
+app.get('/', (req, res) => res.render('pages/login'));
     
 /* changeMap - changes the currently displayed map */
     app.get('/changeMap', function(req, res) {
